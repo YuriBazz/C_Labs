e1c7fb48dd2740f739450eaa8f93471f78693481
Yuri Kovach <108182448+YuriBazz@users.noreply.github.com>
2023-06-04 13:16:28 +0500

    Update file6.c

--- Lab2/file6.c
+++ Lab2/file6.c
@@ -9,7 +9,7 @@ void main(){
     local_Maximum=0;
     k=0;
     scanf("%d", &n);
-    int *array = (int*)malloc((n-1) * sizeof(int));;
+    int *array = (int*)malloc((n) * sizeof(int));;
     for (i=0;i<n;i++){
         printf("Write %d-th element: ", i);
         scanf("%d", &array[i]);
